5d89d2b0c99cba752c118640a73f5a9128aba87e
EL-ZAKARIA <zakaria.elmansouri01@edu.uca.ma>
2023-03-02 03:47:24 -0800

    pointer to the resulting string dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/**
+ * strncat - joins two strings together
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ * Return: identifier for the final string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i = j] = '\0';
+	return (dest);
+}
